c7f9800a63c24ad8b5f5ffe87f7dc8a22b634f6d
Stefano Izzo <45483547+italianpwner@users.noreply.github.com>
2022-12-05 22:15:25 +0100

    Created 'Transaction' class and 'categories.dat' file

---
+++ src/model/Transaction.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.time.LocalDate;
+
+import util.DateUtils;
+import util.Logger;
+
+public class Transaction {
+	
+	static Logger logger = Logger.getInstance();
+	
+	private Integer id;
+	private Double amount;
+	private LocalDate date;
+	private String category;
+	private String description;
+	
+	@SuppressWarnings("unused")
+	private Transaction() {}
+	
+	public Transaction(Integer id, Double amount, LocalDate date,
+			String category, String description)
+	{
+		this.id			= id;
+		this.amount		= amount;
+		this.date		= date;
+		this.category	= category;
+		this.description= description;
+	}
+
+	public static Transaction make(String data, int id) {
+		String[] fields = data.split(";");
+		return new Transaction(
+				id,
+				Double.parseDouble(fields[0]),
+				DateUtils.convert(fields[1]),
+				fields[2],
+				fields[3]
+		);
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"{id: %03d, amount: %8.2f, date %s, category: %-12s, description: '%s'}",
+				id, amount, DateUtils.convert(date), "'"+category+"'", description
+		);
+	}
+}
